968fb8b95b022088a1847b52036d4cc011f2940c
Rothin Roy <mr.rothinroy@gmail.com>
2023-08-08 11:41:11 +0600

    react router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './components/Layout/Home.jsx';
+import Shop from './components/Shop/Shop.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+import cartProductsLoader from './components/loaders/cartProductsLoaders.js';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>,
+      },
+      {
+        path: '/order',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'Login',
+        element: <Login></Login>
+      }
+    ]
+     }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
